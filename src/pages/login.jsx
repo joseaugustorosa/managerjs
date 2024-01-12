786fe8b34d7f95ef779e8456628630011ac489fe
José Augusto Nascimento Rosa <augusto.rosarj@gmail.com>
2024-01-11 21:58:39 -0300

    ajusstes

---
+++ src/pages/login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Login() {
+    return (
+      <div className="bg-sky-700 h-screen w-screen">
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-4/6 h-4/6 bg-slate-50 rounded-2xl m-auto  ">
+                <div>
+                    <h2>Mantenha seu lucro visível.</h2>
+                    <h2>Seu objetivo mais perto de você.</h2>
+                </div>
+                
+
+                <div>
+                <input></input>
+                <input></input>
+                <button>Login</button>
+
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
